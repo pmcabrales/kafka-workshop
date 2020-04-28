9ea803ad1513cb1756f9796fbb838ace4ddc1dc7
pedromartinez <pedro.martinez@kairosds.com>
2020-04-28 16:02:15 +0200

    [UPDATED] Topics and connect configs

--- src/main/java/com/kairos/kafka/workshop/utils/Topics.java
+++ src/main/java/com/kairos/kafka/workshop/utils/Topics.java
@@ -1,6 +1,6 @@
 package com.kairos.kafka.workshop.utils;
 
 public class Topics {
-    public static final String TOPIC_BD_SHOPPING_LIST = "bd_shopping_list";
-    public static final String TOPIC_SHOPPING_LIST = "shopping_list";
+    public static final String TOPIC_BD_SHOPPING_LIST = "jdbc-shopping-list";
+    public static final String TOPIC_SHOPPING_LIST = "shopping-list";
 }
