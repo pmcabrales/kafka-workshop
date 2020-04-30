1ea53f70ce009cc9d26d86da474c96738392f284
pedromartinez <pedro.martinez@kairosds.com>
2020-04-30 10:19:48 +0200

    challenge 4 first commit

--- src/main/java/com/kairos/kafka/workshop/utils/Topics.java
+++ src/main/java/com/kairos/kafka/workshop/utils/Topics.java
@@ -1,6 +1,7 @@
 package com.kairos.kafka.workshop.utils;
 
 public class Topics {
-    public static final String TOPIC_BD_SHOPPING_LIST = "jdbc-shopping-list";
+    //TODO: Change the topic our application is consuming
+    public static final String TOPIC_BD_SHOPPING_LIST = "shopping-list";
     public static final String TOPIC_SHOPPING_LIST = "shopping-list";
 }
